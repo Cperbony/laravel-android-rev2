db583d1e273e67559ebb86fffda6c546a6f93b70
Claudinei Perboni <cperbony@gmail.com>
2018-10-12 00:41:36 -0300

    Iniciando com documentação da API com Swagger

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,10 +6,30 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+/**
+ * Class AuthController
+ * @package App\Http\Controllers\Api
+ * @OA\Info(
+ *     version="0.0.2",
+ *     title="Laravel com Android - Parte I",
+ *     description="API Financeira",
+ * @OA\Contact(email="cperbony@gmail.com")
+ * )
+ */
 class AuthController extends Controller
 {
     use AuthenticatesUsers;
 
+    /**
+     * @param Request $request
+     * @return array|\Illuminate\Http\JsonResponse
+     * @OA\Post(
+     *     path="api/login",
+     *     summary="Login User",
+     *     description="Returns Token JWT",
+     *     @OA\Response(response=200, description="Successfull operation")
+     * )
+     */
     public function login(Request $request)
     {
         $this->validateLogin($request);
@@ -29,12 +49,14 @@ class AuthController extends Controller
             ], 400);
     }
 
-    public function logout(){
+    public function logout()
+    {
         \Auth::guard('api')->logout();
-        return response()->json([],204); //No-content
+        return response()->json([], 204); //No-content
     }
 
-    public function refresh(){
+    public function refresh()
+    {
         $token = \Auth::guard('api')->refresh();
         return ['token' => $token]; //No-content
     }
